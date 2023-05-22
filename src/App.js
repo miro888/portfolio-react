41c7d902d1fe20fa3b8e95a6b1252a2db8d0fee6
Ninochakvetadze <nino.chakvetadze28b@geolab.edu.ge>
2023-05-22 09:51:37 -0700

    added skills

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import './scss/App.scss';
-import person from './userData/userInfo.json'
-
-
+import "./scss/App.scss";
+import person from "./userData/userInfo.json";
 
 function App() {
-  const {firstName, lastName, age, languages, skills, address, job} = person
-  console.log(languages)
+    const { firstName, lastName, age, languages, skills, address, job } =
+        person;
+    console.log(languages);
 
- return (
-    <div className="app">
-     <div className='sidebar left'>
-     
-      <div>
-         <h1>first Name : {firstName} </h1>
-         <h2>last Name : {lastName} </h2>
-         <h2>age : {age} </h2>
-         <h3> address : {address} </h3>
-         <h3> job : {job} Developer </h3>
-         {languages.map((lang, index ) => {
-            return (
-               <div key={index}> 
-                  {lang.language + " level " + lang.level + "%"}
-               </div>
-            )
-         })}
-        
-      </div>
-        person info
-     </div>
-     <div className='middle'>
-        wall
-     </div>
-     <div className='sidebar right'>
-        navigation
-     </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="sidebar left">
+                <div>
+                    <p>First Name: {firstName} </p>
+                    <p>Last Name: {lastName} </p>
+                    <p>Age: {age} </p>
+                    <p>Address: {address} </p>
+                    <p>Job: {job} Developer </p>
+                    <h2>Languages:</h2>
+                    {languages.map((lang, index) => {
+                        return (
+                            <div key={index}>
+                                {lang.language + ": Level " + lang.level + "%"}
+                            </div>
+                        );
+                    })}
+                </div>
+
+                <div>
+                    <h2>Skills:</h2>
+                    {skills.map((skills, index) => {
+                        return (
+                            <div key={index}>
+                                {skills.language +
+                                    ": Level " +
+                                    skills.level +
+                                    "%;"}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className="middle">wall</div>
+            <div className="sidebar right">navigation</div>
+        </div>
+    );
 }
-
 
 export default App;

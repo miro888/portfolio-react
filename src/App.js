5ff2d5457330ccaea8fee3cf332527c558c15c5f
Ninochakvetadze <nino.chakvetadze28b@geolab.edu.ge>
2023-05-22 23:30:47 -0700

    added extra skills, added styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import "./scss/App.scss";
 import person from "./userData/userInfo.json";
 
 function App() {
-    const { firstName, lastName, age, languages, skills, address, job } =
-        person;
+    const {
+        residence,
+        freelance,
+        age,
+        languages,
+        skills,
+        address,
+        job,
+        extraSkills,
+    } = person;
     console.log(languages);
 
     return (
         <div className="app">
-            <div className="sidebar left">
+            <div className="sidebarLeft">
                 <div>
-                    <p>First Name: {firstName} </p>
-                    <p>Last Name: {lastName} </p>
-                    <p>Age: {age} </p>
-                    <p>Address: {address} </p>
-                    <p>Job: {job} Developer </p>
+                    <p>Rayan Adlardard </p>
+                    <p>Dront-End developer</p>
+                </div>
+                <div className="lineDiv"></div>
+                <div className="personInfo">
+                    <p className="ageInfo">Age: {age} </p>
+                    <p className="residenceInfo">Residence: {residence}</p>
+                    <p className="freelanceInfo">Freelance: {freelance}</p>
+                    <p className="addressInfo">Address: {address} </p>
+                    <p className="jobInfo">Job: {job} Developer </p>
+                </div>
+                <div className="lineDiv"></div>
+                <div className="langInfo">
                     <h2>Languages:</h2>
                     {languages.map((lang, index) => {
                         return (
@@ -24,8 +40,8 @@ function App() {
                         );
                     })}
                 </div>
-
-                <div>
+                <div className="lineDiv"></div>
+                <div className="skillsInfo">
                     <h2>Skills:</h2>
                     {skills.map((skills, index) => {
                         return (
@@ -37,6 +53,17 @@ function App() {
                             </div>
                         );
                     })}
+                </div>
+                <div className="lineDiv"></div>
+                <div className="extraSkillsInfo">
+                    <h2>Extra Skills</h2>
+                    {extraSkills.map((skills, index) => {
+                        return <div key={index}>{skills.icon}</div>;
+                    })}
+                </div>
+                <div className="lineDiv"></div>
+                <div>
+                    <button id="downloadCv">DOWNLOAD CV</button>
                 </div>
             </div>
             <div className="middle">wall</div>

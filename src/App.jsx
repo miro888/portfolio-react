cc8c85ed98ae1ea7d82bae12f4e601cdaabb58d1
Giga <giorgi.khundadze15b@geolab.edu.ge>
2023-06-14 03:15:40 +0400

    test update from giga
    inserted new JSON file from other repository

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./scss/App.scss";
 import "./scss/styleOfcompondnts/Dark-Mode/dark-mode.scss";
 import "./scss/styleOfcompondnts/LeftScss/sidebarLeft.scss";
 
-import person from "./userData/userInfo.json";
+// import person from "./userData/userInfo.json";
 import fb from "./images/fb.svg";
 import icon from "./images/icon.svg";
 import insta from "./images/insta.svg";
@@ -19,15 +19,7 @@ import draft5 from "./images/draft5.svg";
 import draft6 from "./images/draft6.svg";
 import draft7 from "./images/draft7.svg";
 import arrow from "./images/arrow.svg";
-
-// import './images/ContactFormICONS/Dot.svg';
-// import '../../images/ContactFormICONS/Email.svg';
-// import'../../images/ContactFormICONS/Flag.svg';
-// import development from "./images/development.svg";
-// import illustrarion from "./images/illustration.svg";
-// import Photographer from "./images/Photographer.svg";
-// import coding from "./images/coding.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
     Buttondiv,
     ExtraSkills,
@@ -45,91 +37,111 @@ function App() {
         const storedDarkMode = localStorage.getItem("darkMode");
         return storedDarkMode ? JSON.parse(storedDarkMode) : "false";
     });
+    const [userInfo, setUserInfo] = useState(null)
+
+    async function setVars(data) {
+
+        setUserInfo(await data)
+        console.log(data)
+    }
+
     const toggledarkMode = () => {
         setdarkMode(!darkMode);
         localStorage.setItem("darkMode", JSON.stringify(!darkMode));
     };
-    const {
-        firstName,
-        lastName,
-        residence,
-        freelance,
-        age,
-        languages,
-        skills,
-        address,
-        job,
-        resumeURL,
-        extraSkills,
-        userImage,
-        social,
-        userImage1,
-        myServiceDescription,
-        services,
-        education,
-        contactinfo,
-        aboutUser,
-        shapes,
-    } = person;
-
+    // const {
+    //     firstName,
+    //     lastName,
+    //     residence,
+    //     freelance,
+    //     age,
+    //     languages,
+    //     skills,
+    //     address,
+    //     job,
+    //     resumeURL,
+    //     extraSkills,
+    //     userImage,
+    //     social,
+    //     userImage1,
+    //     myServiceDescription,
+    //     services,
+    //     education,
+    //     contactinfo,
+    //     aboutUser,
+    //     shapes,
+    // } = person;
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/Gkhundadze/staticFiles/main/personInfo.json')
+            .then((res) => {
+                return res.json()
+            })
+            .then((response) => {
+                setVars(response)
+            })
+    }, [])
     return (
         <div className={`app ${darkMode ? "dark-mode" : "light-mode"}`}>
-            <div className="sidebarLeft">
-                <LeftsideHeader
-                    img={userImage}
-                    name={firstName}
-                    lastName={lastName}
-                    freelanceStatus={freelance}
-                />
-                <Socialicons
-                    link={social.url}
-                    icon_1={fb}
-                    icon_2={insta}
-                    icon_3={tweet}
-                    icon_4={linkdin}
-                    icon_5={yt}
-                    icon_6={icon}
-                />
-                <Personalinfo
-                    age={age}
-                    residence={residence}
-                    jobType={freelance}
-                    location={address}
-                    work={job}
-                />
-                <Languageinfo langs={languages} />
-                <Skills langs={skills} />
-                <ExtraSkills skills={extraSkills} img={square} />
-                <Buttondiv cvUrl={resumeURL} img={download} />
-            </div>
-            <div className="middle">
-                <About
-                    name={firstName}
-                    lastName={lastName}
-                    icon={arrow}
-                    img={userImage1}
-                    aboutUser={aboutUser}
-                    shapes={shapes}
-                />
-                <Myservices
-                    myservice={myServiceDescription}
-                    todoservices={services}
-                />
-                <Education study={education} />
-                <ContactForm info={contactinfo} />
-            </div>
-
-            <div className="sideBarRight">
-                <DarkmodeButton icon={draft7} toggle={toggledarkMode} />
-                <Icons
-                    icon_1={draft1}
-                    icon_2={draft2}
-                    icon_3={draft3}
-                    icon_4={draft4}
-                    icon_5={draft5}
-                    icon_6={draft6}
-                />
-            </div>
+            {userInfo !== null ?
+                <>
+                    <div className="sidebarLeft">
+                        <LeftsideHeader
+                            img={userInfo.userImage}
+                            name={userInfo.firstName}
+                            lastName={userInfo.lastName}
+                            freelanceStatus={userInfo.freelance}
+                        />
+                        <Socialicons
+                            link={userInfo.social.url}
+                            icon_1={fb}
+                            icon_2={insta}
+                            icon_3={tweet}
+                            icon_4={linkdin}
+                            icon_5={yt}
+                            icon_6={icon}
+                        />
+                        <Personalinfo
+                            age={userInfo.age}
+                            residence={userInfo.residence}
+                            jobType={userInfo.freelance}
+                            location={userInfo.address}
+                            work={userInfo.job}
+                        />
+                        <Languageinfo langs={userInfo.languages} />
+                        <Skills langs={userInfo.skills} />
+                        <ExtraSkills skills={userInfo.extraSkills} img={square} />
+                        <Buttondiv cvUrl={userInfo.resumeURL} img={download} />
+                    </div>
+                    <div className="middle">
+                        <About
+                            name={userInfo.firstName}
+                            lastName={userInfo.lastName}
+                            icon={arrow}
+                            img={userInfo.userImage1}
+                            aboutUser={userInfo.aboutUser}
+                            shapes={userInfo.shapes}
+                        />
+                        <Myservices
+                            myservice={userInfo.myServiceDescription}
+                            todoservices={userInfo.services}
+                        />
+                        <Education study={userInfo.education} />
+                        <ContactForm info={userInfo.contactinfo} />
+                    </div>
+                    <div className="sideBarRight">
+                        <DarkmodeButton icon={draft7} toggle={toggledarkMode} />
+                        <Icons
+                            icon_1={draft1}
+                            icon_2={draft2}
+                            icon_3={draft3}
+                            icon_4={draft4}
+                            icon_5={draft5}
+                            icon_6={draft6}
+                        />
+                    </div>
+                </>
+                : null
+            }
         </div>
     );
 }
